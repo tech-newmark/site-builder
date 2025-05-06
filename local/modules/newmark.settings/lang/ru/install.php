eb72f1ea250984e746633e225c0f518619672cfa
Yuriy Bevov <yuriybevov@gmail.com>
2025-05-06 20:55:36 +0300

    Добавил в админку модуль настроек шаблона

--- local/modules/newmark.settings/lang/ru/install.php
+++
@@ -1,3 +0,0 @@
-<?php
-$MESS['NEWMARK_SETTINGS_INSTALL_NAME'] = 'Настройки NewMark';
-$MESS['NEWMARK_SETTINGS_INSTALL_DESCRIPTION'] = 'Модуль глобальных настроек шаблона сайта';
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,16 +1,45 @@
 <?php
-AddEventHandler("main", "OnBuildGlobalMenu", "addNewmarkSettingsMenu");
 
-function addNewmarkSettingsMenu(&$arGlobalMenu, &$arModuleMenu)
+use Bitrix\Main\EventManager;
+use Bitrix\Main\IO;
+
+// Регистрация обработчика события
+EventManager::getInstance()->addEventHandler(
+  'main',
+  'OnBuildGlobalMenu',
+  'addNewmarkSettingsMenuD7'
+);
+
+function addNewmarkSettingsMenuD7(&$arGlobalMenu, &$arModuleMenu)
 {
-  if ($GLOBALS['USER']->IsAdmin()) {
-    $arModuleMenu[] = [
-      "parent_menu" => "global_menu_settings",
-      "section"     => "newmark_settings",
-      "sort"        => 90,
-      "text"        => "Настройки NewMark",
-      "title"       => "Глобальные настройки шаблона сайта",
-      "url"         => "/local/modules/newmark.settings/newmark_settings.php",
-    ];
+  global $USER;
+
+  if (!$USER->IsAdmin()) {
+    return;
   }
+
+  // Пути
+  $sourceFile = $_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/templates/nmark_simple_settings_template.php";
+  $targetFile = $_SERVER["DOCUMENT_ROOT"] . "/bitrix/admin/nmark_simple.php";
+
+  // Копируем файл, если его нет
+  if (!file_exists($targetFile)) {
+    if (file_exists($sourceFile)) {
+      $file = new IO\File($sourceFile);
+      $file->copy($targetFile);
+    } else {
+      // Логирование или вывод ошибки
+      throw new \Exception("Файл шаблона не найден");
+    }
+  }
+
+  // Добавляем пункт меню
+  $arModuleMenu[] = [
+    "parent_menu" => "global_menu_settings",
+    "section"     => "newmark_settings",
+    "sort"        => 90,
+    "text"        => "NewMark Simple v1.0.0",
+    "title"       => "Настройки шаблона NewMark Simple v1.0.0",
+    "url"         => "/bitrix/admin/nmark_simple.php",
+  ];
 }
