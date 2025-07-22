a86ef53e80adb085364e37544700a36a5214e591
Yuriy Bevov <yuriybevov@gmail.com>
2025-07-22 13:06:50 +0300

    Добавил блок с аккордеоном

--- local/frontend/src/js/main.js
+++ local/frontend/src/js/main.js
@@ -2,3 +2,4 @@
 
 import "@pug/components/_ui/sliders/top-banner/script.js";
 import "@pug/components/_ui/sliders/infinity-row-slider/script.js";
+import "@pug/components/_ui/accordeon/accordeon.js";
