6e1045a8c08eef22c3d7ad3c44a709bd3b7f4668
Yuriy Bevov <yuriybevov@gmail.com>
2025-07-22 17:58:30 +0300

    блок со слайдером сотрудников

--- local/frontend/src/js/main.js
+++ local/frontend/src/js/main.js
@@ -3,3 +3,4 @@
 import "@pug/components/_ui/sliders/top-banner/script.js";
 import "@pug/components/_ui/sliders/infinity-row-slider/script.js";
 import "@pug/components/_ui/accordeon/accordeon.js";
+import "@pug/components/_ui/sliders/main-slider/script.js";
