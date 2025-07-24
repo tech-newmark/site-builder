96cca78677ec21a6f25d50f84a7c7a2da25d5199
Yuriy Bevov <yuriybevov@gmail.com>
2025-07-24 10:22:39 +0300

    Блок контакты

--- local/frontend/src/pug/components/_ui/sliders/main-slider/script.js
+++ local/frontend/src/pug/components/_ui/sliders/main-slider/script.js
@@ -2,7 +2,6 @@ import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-// import "swiper/css/navigation";
 
 const sliders = document.querySelectorAll(".main-slider");
 
