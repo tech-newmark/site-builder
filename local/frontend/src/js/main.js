f2948936ff4575862b428dcf5f0aba74311fd7f7
Yuriy Bevov <yuriybevov@gmail.com>
2025-07-24 16:52:51 +0300

    Добавил раздел контакты

--- local/frontend/src/js/main.js
+++ local/frontend/src/js/main.js
@@ -5,4 +5,4 @@ import "@pug/components/_ui/sliders/infinity-row-slider/script.js";
 import "@pug/components/_ui/accordeon/accordeon.js";
 import "./modules/table-wrapper";
 import "@pug/components/_ui/sliders/main-slider/script.js";
-
+// import "@pug/components/_ui/yMaps/script.js";
