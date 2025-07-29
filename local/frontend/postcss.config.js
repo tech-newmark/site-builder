163cd0f9da0429086334e66a8416ba4c98864add
Yuriy Bevov <yuriybevov@gmail.com>
2025-07-29 11:55:50 +0300

    Добавил конф файл для претьера

--- local/frontend/postcss.config.js
+++ local/frontend/postcss.config.js
@@ -6,30 +6,30 @@ import postcssCalc from "postcss-calc";
 import postcssDiscardComments from "postcss-discard-comments";
 
 export default {
-	plugins: [
-		postcssCalc({}),
-		autoprefixer({
-			overrideBrowserslist: ["last 5 versions"],
-		}),
-		postcssSortMediaQueries({
-			sort: "mobile-first", // mobile-first || desktop-first
-		}),
-		// purgecss({
-		//   content: ['./**/*.html', './**/*.pug', './**/*.css'],
-		//   safelist: {
-		//     standard: [/no-image/],
-		//     deep: [/^swiper/, /^zoom/, /^ymaps-icon-content-layout-inner/, /^fancybox/],
-		//   }
-		// }),
-		// always after postcssSortMediaQueries
-		postcssCombineDuplicatedSelectors({
-			removeDuplicatedValues: true,
-		}),
-		postcssDiscardComments({
-			removeAll: true,
-		}),
-	],
-	options: {
-		cascade: true,
-	},
+  plugins: [
+    postcssCalc({}),
+    autoprefixer({
+      overrideBrowserslist: ["last 5 versions"],
+    }),
+    postcssSortMediaQueries({
+      sort: "mobile-first", // mobile-first || desktop-first
+    }),
+    // purgecss({
+    //   content: ['./**/*.html', './**/*.pug', './**/*.css'],
+    //   safelist: {
+    //     standard: [/no-image/],
+    //     deep: [/^swiper/, /^zoom/, /^ymaps-icon-content-layout-inner/, /^fancybox/],
+    //   }
+    // }),
+    // always after postcssSortMediaQueries
+    postcssCombineDuplicatedSelectors({
+      removeDuplicatedValues: true,
+    }),
+    postcssDiscardComments({
+      removeAll: true,
+    }),
+  ],
+  options: {
+    cascade: true,
+  },
 };
