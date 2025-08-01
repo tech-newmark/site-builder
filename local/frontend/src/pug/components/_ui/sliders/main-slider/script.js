ca6032b627b8d9f9210d35e262448df910b1fefd
Oksana Tarasova <oksanchik_90@mail.ru>
2025-08-01 14:48:17 +0300

    Верстка блока с отзывами

--- local/frontend/src/pug/components/_ui/sliders/main-slider/script.js
+++ local/frontend/src/pug/components/_ui/sliders/main-slider/script.js
@@ -6,40 +6,67 @@ import "swiper/css/pagination";
 const sliders = document.querySelectorAll(".main-slider");
 
 if (sliders.length) {
-	sliders.forEach((slider) => {
-		const btnNext = slider.parentNode.querySelector(".swiper-button-next");
-		const btnPrev = slider.parentNode.querySelector(".swiper-button-prev");
-		const pagination = slider.parentNode.querySelector(".swiper-pagination");
+  const breakpoints = (slider) => {
+    if (slider.classList.contains("staff-preview-slider")) {
+      return {
+        680: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      };
+    } else if (slider.classList.contains("reviews-slider")) {
+      return {
+        680: {
+          slidesPerView: slider.classList.contains("--max-slides-2")
+            ? 1
+            : slider.classList.contains("--max-slides-3")
+            ? 2
+            : 1,
+          spaceBetween: slider.classList.contains("--max-slides-3") ? 32 : 20,
+        },
+        1024: {
+          slidesPerView: slider.classList.contains("--max-slides-2")
+            ? 2
+            : slider.classList.contains("--max-slides-3")
+            ? 3
+            : 1,
+          spaceBetween: slider.classList.contains("--max-slides-2")
+            ? 64
+            : slider.classList.contains("--max-slides-3")
+            ? 32
+            : 20,
+        },
+      };
+    }
+  };
 
-		new Swiper(slider, {
-			modules: [Navigation, Pagination],
-			slidesPerView: slider.classList.contains("--js--auto-fill") ? "auto" : 1,
-			spaceBetween: 20,
-			centeredSlidesBounds: true,
+  sliders.forEach((slider) => {
+    const btnNext = slider.parentNode.querySelector(".swiper-button-next");
+    const btnPrev = slider.parentNode.querySelector(".swiper-button-prev");
+    const pagination = slider.parentNode.querySelector(".swiper-pagination");
 
-			breakpoints: {
-				680: {
-					slidesPerView: slider.classList.contains("--js--auto-fill")
-						? "auto"
-						: 2,
-				},
-				1024: {
-					slidesPerView: slider.classList.contains("--js--auto-fill")
-						? "auto"
-						: 3,
-				},
-			},
+    new Swiper(slider, {
+      modules: [Navigation, Pagination],
+      slidesPerView: slider.classList.contains("--js--auto-fill") ? "auto" : 1, // start from mobile
+      spaceBetween: 20,
+      centeredSlidesBounds: true,
 
-			navigation: {
-				nextEl: btnNext ? btnNext : null,
-				prevEl: btnPrev ? btnPrev : null,
-			},
+      breakpoints: !slider.classList.contains("--js--auto-fill")
+        ? breakpoints(slider)
+        : null,
 
-			pagination: {
-				el: pagination ? pagination : null,
-				dynamicBullets: pagination && pagination.dataset.dynamic ? true : false,
-				clickable: true,
-			},
-		});
-	});
+      navigation: {
+        nextEl: btnNext ? btnNext : null,
+        prevEl: btnPrev ? btnPrev : null,
+      },
+
+      pagination: {
+        el: pagination ? pagination : null,
+        dynamicBullets: pagination && pagination.dataset.dynamic ? true : false,
+        clickable: true,
+      },
+    });
+  });
 }
