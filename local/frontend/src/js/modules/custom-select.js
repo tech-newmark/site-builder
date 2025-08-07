4dbef37a6d751efe5bb9cc51615724f817022f45
Yuriy Bevov <yuriybevov@gmail.com>
2025-08-07 16:16:23 +0300

    УДАЛИЛ ПАПКУ FRONTEND

--- local/frontend/src/js/modules/custom-select.js
+++
@@ -1,3 +0,0 @@
-import customSelect from "custom-select";
-
-export let cstSel = customSelect(".custom-select");
--- local/frontend/src/js/modules/fancy.js
+++
@@ -1,8 +0,0 @@
-import { Fancybox } from "@fancyapps/ui";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
-
-const fancy = document.querySelectorAll('[data-fancybox]');
-
-if(fancy.length) {
-  Fancybox.bind("[data-fancybox]", {});
-}
